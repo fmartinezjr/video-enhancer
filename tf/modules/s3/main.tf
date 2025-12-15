88533d9ec57283f86c31ccdd97ee7e41baf8f5e5
Francisco Martinez <franciscomj345@gmail.com>
2025-12-15 16:06:39 -0700

    s3

---
+++ tf/modules/s3/main.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "videos" {
+  bucket = var.app_name
+  tags   = var.tags
+}
+
+# Enable versioning
+resource "aws_s3_bucket_versioning" "videos" {
+  bucket = aws_s3_bucket.videos.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Block public access
+resource "aws_s3_bucket_public_access_block" "videos" {
+  bucket = aws_s3_bucket.videos.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
